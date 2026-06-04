f6b4a6c6742f42bc8a93a8a06a0a0e5ad09d9808
ZISHUN GAO <144342588+ZishunD@users.noreply.github.com>
2026-06-04 09:35:00 +0700

    更新 Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (<>
         <div className="flex justify-center my-[10vh] transition-all duration-500 ease-in-out">
-            <p className="text-[#96614F]">© 2025 Zishun Gao</p>
+            <p className="text-[#96614F]">© 2026 Zishun Gao</p>
         </div>
     </>)
 }
